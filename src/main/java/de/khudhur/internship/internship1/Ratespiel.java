b06d6fc81933b5b7d8535fc369cf4db8325de1b4
Ayaz Khudhur <72782505+khudhurayaz@users.noreply.github.com>
2022-10-31 17:20:31 +0100

    create internship and Ratespiel

---
+++ src/main/java/de/khudhur/internship/internship1/Ratespiel.java
@@ -0,0 +1,39 @@
+package de.khudhur.internship.internship1;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Ratespiel {
+
+    public static void main(String[] args){
+        rateZahl();
+    }
+
+    private static void rateZahl(){
+        int randomNumber = (int) (Math.random() * 101.0); //[0..100]
+        int userNumber = 0;
+        int anzahlAnVersuchen = 0;
+        Scanner scanner = new Scanner(System.in());
+        
+        do {
+            System.out.println("Zahl: ");
+            userNumber = scanner.nextInt;
+
+            if(randomNumber > userNumber) {
+                System.out.println("Zufallszahl ist kleiner!");
+                anzahlAnVersuchen++;
+            }else if(randomNumber < userNumber) {
+                System.out.println("Zufallszahl ist groesser!");
+                anzahlAnVersuchen++;
+            }else if(randomNumber == userNumber){
+                System.out.println("Geschafft! Zufallszahl war " + randomNumber);
+                System.out.println("Anzahl an Versuchen = " + anzahlAnVersuchen);
+                break;
+            }else {
+                System.out.println("Fehler!");
+            }
+
+        }while(randomNumber != userNumber);
+
+    }
+}
