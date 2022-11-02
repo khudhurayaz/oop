d9d7c79560473192db773e631eac129fcac7725f
Ayaz Khudhur <72782505+khudhurayaz@users.noreply.github.com>
2022-11-02 13:24:07 +0100

    create sensordaten

---
+++ src/main/java/de/khudhur/internship/internship1/Sensordaten.java
@@ -0,0 +1,16 @@
+public class Sensordaten {
+    public static void main(String[] args){
+        filterMesswerte(1);
+    }
+
+    private static void filterMesswerte(int wert) {
+        int nullen = 0;
+        if(wert <= 0){
+            if(wert == 0){
+                nullen += 0;
+            }
+        }
+
+        if()
+    }
+} 
