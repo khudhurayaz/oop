c7f15bc9f9ce36d751b82f58e8126d52b600e52c
khudhurAyaz <ayazshary37@gmail.com>
2022-11-03 16:34:36 +0100

    Comment the median class

--- src/main/java/de/khudhur/internship/internship3/Median.java
+++ src/main/java/de/khudhur/internship/internship3/Median.java
@@ -4,24 +4,38 @@ import java.util.Arrays;
 
 public class Median {
     public static void main(String[] args) {
-        int[] array1 = { 12, 16, 2, 63, 55, 8 };
-        int[] array2 = { 42, 7, 543, 77, 87, 876, 143 };
-        System.out.println("Median für Array 1: " + median(array1));
-        System.out.println("Median für Array 2: " + median(array2));
+        int[] array1 = { 12, 16, 2, 63, 55, 8 }; // Test array 1 gerade Zahl
+        int[] array2 = { 42, 7, 543, 77, 87, 876, 143 }; // Test array 2 ungerade Zahl
+        System.out.println("Median für Array 1: " + median(array1) + "\n"); // Output array 1
+        System.out.println("Median für Array 2: " + median(array2) + "\n"); // Output array 2
     }
 
-    private static int median(int[] array) {
+    /**
+     * @param array erwartet einen Integer Array
+     * @return ein integer Zahl als Rückgabewert
+     */
+    private static int median(int[] array) { // Anfang
 
         Arrays.sort(array); // array sortieren
+        System.out.println("Array: ");
+
+        // Array output
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();// blank line
 
+        // operation: prüfe ob modul 2 gleich null ist
         if (array.length % 2 == 0) {
+            // dann die beiden mittleren Zahlen zusammenaddieren und durch Zwei teilen.
             return (int) ((array[array.length / 2] + array[(array.length / 2) - 1]) / 2);
         } else {
+            /**
+             * ansonsten teile die array länge durch 2 + 1 {0.n}
+             * und das ganze durch nochmal durch 2 teilen, um den mittleren wert zu bekommen
+             */
             return array[array.length / 2] + 1 / 2;
         }
+    } // Ende
 
-    }
 }
