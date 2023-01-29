c2973025ab844057fd82d23001ba140b190f4d99
khudhurayaz <ayazshary37@gmail.com>
2023-01-29 20:05:34 +0100

    Hobby Projekten

---
+++ src/main/java/de/khudhur/projects/AdditionDual/AdditionDual.java
@@ -0,0 +1,47 @@
+package de.khudhur.projects.AdditionDual;
+
+import java.util.Scanner;
+
+public class AdditionDual {
+    public static void main(String[] args) {
+        String add1 = "";
+        String add2 = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Addition Binär 1: ");
+        add1 = scanner.nextLine();
+        System.out.println("Addition Binär 2: ");
+        add2 = scanner.nextLine();
+
+        StringBuilder sum = new StringBuilder();
+        char[] array1 = new char[add1.length()];
+        char[] array2 = new char[add2.length()];
+        for (int i = 0; i < add1.length(); i++) {
+            array1[i] = add1.charAt(i);
+        }
+        for (int i = 0; i < add2.length(); i++) {
+            array2[i] = add2.charAt(i);
+        }
+
+        //prüfung
+        for (int i = 0; i < array1.length; i++) {
+            int uebertragung = 0;
+            if (array1[i] == '0' && array2[i] == '0' || array2[i] == '0' && array1[i] == '0'){
+                sum.append(0);
+            }
+            if (array1[i] == '0' && array2[i] == '1' || array2[i] == '0' && array1[i] == '1'){
+                sum.append(1);
+            }
+            if (array1[i] == '1' && array2[i] == '1' || array2[i] == '1' && array1[i] == '1'){
+                if (uebertragung == 1){
+                    sum.append(1);
+                }else {
+                    sum.append(0);
+                }
+                uebertragung = 1;
+            }
+        }
+
+        //ausgabe
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/de/khudhur/projects/ggT/GGT.java
@@ -0,0 +1,83 @@
+package de.khudhur.projects.ggT;
+
+import javax.swing.*;
+
+/**
+ *<h1>Algorithmus von Euklid (ggT)</h1>
+ * <p>
+ *     Der Algorithmus von Euklid ist ein Verfahren zur Bestimmung
+ *     des größten gemeinsamen Teilers (ggT) zweier oder mehrerer natürlicher Zahlen.
+ *     <br>
+ *     Der Algorithmus ist nach dem Mathematiker Euklid benannt,
+ *     der ihn bereits im 3.Jahrhundert v.Chr. beschrieben hat.
+ * </p>
+ * @author Ayaz Khudhur
+ * @version 1.0
+ */
+public class GGT {
+    public static void main(String[] args) {
+        //Zeichenkette für a und b
+        String strA = "", strB = "";
+        //Ganzzahlen für a, b und r
+        int a = 0, b = 0, r = 0;
+        //boolischeswert für weiter
+        boolean weiter = true;
+        //User eingabe
+        while (weiter){
+            //Eingabe für a
+            strA = JOptionPane.showInputDialog("Gib ein Zahl für Zahl 1 ein: ");
+            //Eingabe für b
+            strB = JOptionPane.showInputDialog("Gib ein Zahl für Zahl 2 ein: ");
+            //in Ganzzahlen umwandeln
+            a = Integer.parseInt(strA);
+            b = Integer.parseInt(strB);
+
+            //abfrage ob, in a und b nur positiven werten sind
+            if (pruefe(a, b)) {
+                continue;
+            }else {
+                weiter = false;
+            }
+        }
+
+        //ggT errechnen
+        do {
+            r = a % b;
+            a = b;
+            b = r;
+
+        }while (r != 0);
+
+        //ausgabe
+        JOptionPane.showMessageDialog(
+                null,
+                "---->ggT von a = " + strA + " und b = " + strB + ", ist: " + a,
+                "ggT von Euklid",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static boolean pruefe(int...zahl){
+        boolean weiter = false;
+        for (int i : zahl) {
+            if (i == 0 || i == -1){
+                int option = JOptionPane.showConfirmDialog(
+                        null,
+                        "Dürfen nullen oder negativen Zahlen nicht enthalten!",
+                        "ggT von Euklid",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+                if (option == JOptionPane.OK_OPTION) {
+                    weiter = true;
+                }else {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Programm wird beendet!",
+                            "ggT von Euklid",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    System.exit(-1);
+                    weiter = false;
+                }
+            }
+        }
+        return weiter;
+    }
+}
